d0f09ab1c071cf69005ed6bae10eb2985d8477c8
Rafael Oliveira da Silva <rafaeloliveiradasilva@Rafaels-MacBook-Pro.local>
2022-07-13 13:50:08 -0300

    Refactored by Rafa MenuAccess.Java

--- src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
@@ -1,34 +1,36 @@
 package com.c.refactoring.menuexamples;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MenuAccess {
 
-    public void setAuthorizationsInEachMenus(
-            List<MenuItem> menuItemsList, Role[] roles) {
-        for (int i = 0; i < menuItemsList.size(); i++) {
-            MenuItem menuItem = menuItemsList.get(i);
-            if (roles != null) {
-                for (int j = 0; j < roles.length; j++) {
-                    if (roles[j].getName().equals(menuItem
-                            .getReadAccessRole())
-                            && !Constants.WRITE.equals(
-                                    menuItem
-                                            .getAccess())) {
-                        menuItem.setAccess(Constants.READ);
-                        menuItem.setVisible(true);
-                    } else if (roles[j].getName().equals(
-                            menuItem
-                                    .getWriteAccessRole())) {
-                        menuItem.setAccess(Constants.WRITE);
-                        menuItem.setVisible(true);
-                    }
-                }
+	public void setAuthorizationsInEachMenus(List<MenuItem> menuItemsList, Role[] roles) {
 
-            }
+		if (roles == null) {
+			return;
+		}
 
-        }
+		menuItemsList.forEach(menuItem -> {
 
-    }
+			if (hasRole(roles, menuItem.getReadAccessRole())) {
+				menuItem.setAccess(Constants.READ);
+				menuItem.setVisible(true);
+
+			}
+			if (hasRole(roles, menuItem.getWriteAccessRole())) {
+				menuItem.setAccess(Constants.WRITE);
+				menuItem.setVisible(true);
+			}
+
+		});
+
+	}
+
+	private boolean hasRole(Role[] roles, String typeAccessRole) {
+
+		return Arrays.stream(roles).anyMatch(role -> role.getName().equals(typeAccessRole));
+
+	}
 
 }
